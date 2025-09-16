b76d3810bcb92b7a003243e615cc6e9dbf9a4f38
Hakan Or <hor@invio.com>
2025-09-16 17:43:23 +0300

    bypass logger

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -21,7 +21,6 @@ let package = Package(
     dependencies: [
         // LK-Prefixed Dynamic WebRTC XCFramework
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.29.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.2"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0"),
         // Only used for DocC generation
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0"),
@@ -39,7 +38,6 @@ let package = Package(
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 .product(name: "DequeModule", package: "swift-collections"),
                 .product(name: "OrderedCollections", package: "swift-collections"),
-                .product(name: "Logging", package: "swift-log"),
                 "LKObjCHelpers",
             ],
             exclude: [
