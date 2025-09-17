736789abbe58a57a57bcc789f373d7c232b25ce4
Hakan Or <hor@invio.com>
2025-09-17 18:24:00 +0300

    remove logging

--- Package.swift
+++ Package.swift
@@ -20,12 +20,7 @@ let package = Package(
     dependencies: [
         // LK-Prefixed Dynamic WebRTC XCFramework
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.29.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.2"),
-        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0"),
-        // Only used for DocC generation
-        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0"),
-        // Only used for Testing
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.13.4"),
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0")
     ],
     targets: [
         .target(
@@ -38,7 +33,6 @@ let package = Package(
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 .product(name: "DequeModule", package: "swift-collections"),
                 .product(name: "OrderedCollections", package: "swift-collections"),
-                .product(name: "Logging", package: "swift-log"),
                 "LKObjCHelpers",
             ],
             exclude: [
@@ -50,21 +44,7 @@ let package = Package(
             swiftSettings: [
                 .enableExperimentalFeature("AccessLevelOnImport"),
             ]
-        ),
-        .testTarget(
-            name: "LiveKitTests",
-            dependencies: [
-                "LiveKit",
-                .product(name: "JWTKit", package: "jwt-kit"),
-            ]
-        ),
-        .testTarget(
-            name: "LiveKitTestsObjC",
-            dependencies: [
-                "LiveKit",
-                .product(name: "JWTKit", package: "jwt-kit"),
-            ]
-        ),
+        )
     ],
     swiftLanguageVersions: [
         .v5,
--- Sources/LiveKit/Broadcast/Support/OSLogHandler.swift
+++ Sources/LiveKit/Broadcast/Support/OSLogHandler.swift
@@ -18,7 +18,6 @@
 
 import Foundation
 import OSLog
-internal import Logging
 
 struct OSLogHandler: LogHandler {
     var logLevel: Logging.Logger.Level = .debug
